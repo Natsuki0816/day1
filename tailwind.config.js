3082a7712c2eafaad42e81fe5bef1d4e6a9d5b19
Natsuki0816 <happi68end@gmail.com>
2022-05-31 17:08:19 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '640px', // ここを変更
+      md: '768px', // ここは追加
+      lg: '1024px',// ここは追加
+      xl: '1280px'// ここは追加
+    },
+    
+    },
+    plugins: [],
+  };
+
